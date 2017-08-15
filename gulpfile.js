c1ca1fbf113f9b7c5759e9b6ac452f2be65f5056
Krzysiek Kuczma <kuczma.krzysiek@gmail.com>
2017-08-15 16:34:31 +0200

    Przygotowane do pracy z Warsztatem 3

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('serve',function() {
+    browserSync.init({
+        server: "./",
+        notify: false
+    });
+});
+
+var handleError = function (err){
+    console.log(err.toString());
+    this.emit('end');
+}
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/main.scss')
+  .pipe(plumber({
+      errorHandler: handleError
+    }))
+    .pipe(sourcemaps.init())
+        .pipe(sass())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});
+gulp.task('scripts', function() {
+  return gulp.src('./src/scripts/**/*.js')
+    .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./js/'));
+});
+gulp.task('watch', function(){
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/scripts/**/*.js', ['scripts']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+gulp.task('default', function() {
+	console.log('Uruchamam domyślne - zadanie gulpa');
+    gulp.start(['serve', 'watch', 'scripts']);
+});
---
+++ js/app.js
@@ -0,0 +1,3 @@
+console.log('Test działania')
+
+//# sourceMappingURL=app.js.map
